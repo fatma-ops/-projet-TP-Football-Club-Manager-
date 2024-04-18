456a7343cfa3ae5569e0ea45ac40b05618944b60
Moranoo <92519044+Moranoo@users.noreply.github.com>
2024-04-18 12:36:06 +0200

    Mise à jour routes et models et add test.http

--- backend/src/models/Equipe.ts
+++ backend/src/models/Equipe.ts
@@ -1,18 +1,19 @@
+// Importez User si ce n'est pas déjà fait
 import mongoose, { Schema, Document } from 'mongoose';
-import {IJoueur} from "./Joueur";
+import { IJoueur } from "./Joueur";
+import { IUtilisateur } from "./User"; // Assurez-vous que le chemin d'importation est correct
 
 export interface IEquipe extends Document {
-    id: string;
     nom: string;
     budget: number;
-    manager: string;
+    manager: IUtilisateur;
     joueurs: IJoueur[];
 }
 
 const equipeSchema = new Schema({
     nom: { type: String, required: true },
     budget: { type: Number, required: true },
-    manager: { type: String, required: true },
+    manager: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Référence au modèle User
     joueurs: [{ type: Schema.Types.ObjectId, ref: 'Joueur' }]
 });
 

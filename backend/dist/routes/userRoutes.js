0852918811e52eee81b15bb420d846030d81fcb8
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2024-04-18 12:01:03 +0100

    token

--- backend/dist/routes/userRoutes.js
+++ backend/dist/routes/userRoutes.js
@@ -1,5 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const User_1 = __importDefault(require("../models/User"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const router = (0, express_1.Router)();
+// Signup
+router.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { nom, email, motDePasse, role } = req.body;
+        // Vérifiez si l'utilisateur existe déjà
+        const existingUser = yield User_1.default.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Cet utilisateur existe déjà.' });
+        }
+        // Hash du mot de passe
+        const saltRounds = 10;
+        const hashedPassword = yield bcrypt_1.default.hash(motDePasse, saltRounds);
+        // Création de l'utilisateur
+        const newUser = new User_1.default({
+            nom,
+            email,
+            motDePasse: hashedPassword,
+            role,
+            club: req.body.club
+        });
+        const savedUser = yield newUser.save();
+        res.status(201).json(savedUser);
+    }
+    catch (error) {
+        console.error('Erreur lors de la création de l\'utilisateur:', error);
+        res.status(500).json({ message: 'Une erreur est survenue lors de la création de l\'utilisateur.' });
+    }
+}));
 exports.default = router;
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,6 +1,45 @@
 import { Router, Request, Response } from "express";
+import UserModel , {IUtilisateur} from "../models/User"
+import bcrypt from 'bcrypt';
+
 const router = Router();
 
+// Signup
+
+
+router.post('/signup', async (req: Request, res: Response) => {
+  try {
+    const { nom, email, motDePasse, role } = req.body;
+
+    // Vérifiez si l'utilisateur existe déjà
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Cet utilisateur existe déjà.' });
+    }
+
+    // Hash du mot de passe
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(motDePasse, saltRounds);
+
+    // Création de l'utilisateur
+    const newUser: IUtilisateur = new UserModel({
+      nom ,
+      email,
+      motDePasse: hashedPassword,
+      role ,
+      club: req.body.club 
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    console.error('Erreur lors de la création de l\'utilisateur:', error);
+    res.status(500).json({ message: 'Une erreur est survenue lors de la création de l\'utilisateur.' });
+  }
+});
+
+export default router;
 
 
 
@@ -16,4 +55,12 @@ const router = Router();
 
 
 
-export default router ;             
+
+
+
+
+
+
+
+
+

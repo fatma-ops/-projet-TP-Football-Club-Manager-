5826d5eccd331692cba0ac3ba14484d330f15b63
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2024-04-18 22:23:50 +0100

    Connexion + inscription

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
-import User , {IUtilisateur} from "../models/User";
+import User , {IUtilisateur}  from "../models/User";
+import { signUp  } from '../controller/userController';
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const router = Router();
+
 
 router.get('/', async (req: Request, res: Response) => {
     const users = await User.find();
@@ -14,46 +17,36 @@ router.get('/', async (req: Request, res: Response) => {
 
 router.post('/signup', async (req: Request, res: Response) => {
     try {
-      const { nom, email, motDePasse, role, club } = req.body;
-  
-      if (!nom || !email || !motDePasse || !role || !club) {
-        return res.status(400).json({ message: 'Tous les champs sont requis.' });
-      }
-  
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: 'Adresse e-mail invalide.' });
-      }
-  
-      if (motDePasse.length < 8) {
-        return res.status(400).json({ message: 'Le mot de passe doit comporter au moins 8 caractères.' });
-      }
-  
-  
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Cette adresse e-mail est déjà utilisée.' });
-      }
-  
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(motDePasse, saltRounds);
-  
-      const newUser: IUtilisateur = new User({
-        nom,
-        email,
-        motDePasse: hashedPassword,
-        role,
-        club
-      });
-  
-      const savedUser = await newUser.save();
-  
-      res.status(201).json(savedUser);
+      const user = await signUp(req, res); 
+      res.status(201).json(user); 
     } catch (error) {
       console.error('Erreur lors de la création de l\'utilisateur:', error);
       res.status(500).json({ message: 'Une erreur est survenue lors de la création de l\'utilisateur.' });
     }
   });
+
+  router.post('/signin', async (req: Request, res: Response) => {
+    try {
+        const { email, motDePasse }: Pick<IUtilisateur, "email" | "motDePasse"> = req.body;
+
+        const utilisateur = await User.findOne({ email });
+        if (!utilisateur) {
+            return res.status(400).json({ message: "L'email  est incorrect." });
+        }
+
+        const motDePasseValide = await bcrypt.compare(motDePasse, utilisateur.motDePasse);
+        if (!motDePasseValide) {
+            return res.status(400).json({ message: "le mot de passe est incorrect." });
+        }
+
+        res.status(200).json({ message: "Connexion réussie." });
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
 router.get('/:id', async (req: Request, res: Response) => {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -64,8 +57,8 @@ router.get('/:id', async (req: Request, res: Response) => {
 
 router.put('/:id', async (req: Request, res: Response) => {
     try {
-        const { name, email, motDePasse, role, club } = req.body;
-        const user = await User.findByIdAndUpdate(req.params.id, { name, email, motDePasse, role, club }, { new: true });
+        const { name, email, motDePasse, isAdmin, club } = req.body;
+        const user = await User.findByIdAndUpdate(req.params.id, { name, email, motDePasse, isAdmin, club }, { new: true });
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
@@ -83,4 +76,4 @@ router.delete('/:id', async (req: Request, res: Response) => {
     res.status(204).send();
 });
 
-export default router;
+export default router ;
